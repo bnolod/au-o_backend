1e709983ecc422d646b685d14e22640fb30e1644
bnolod <sadfoolc@gmail.com>
2025-01-13 08:34:47 +0100

    repository added

--- src/main/java/com/auo/backend/user/UserService.java
+++ src/main/java/com/auo/backend/user/UserService.java
@@ -1,23 +1,25 @@
 package com.auo.backend.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class UserService {
-    public List<User> getUsers() {
-        return List.of(new User(
-                1L,
-                "felh1",
-                "nick",
-                "jelszo",
-                3,
-                "user@email.com",
-                true,
-                "imgsrc",
-                "biotext"
 
-        ));
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    public User postUser(User user) {
+        return userRepository.save(user);
     }
 }
