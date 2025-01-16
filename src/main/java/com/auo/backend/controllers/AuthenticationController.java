bcf00a2115a5021401db8d0a9a93c0c917db7723
bnolod <sadfoolc@gmail.com>
2025-01-16 09:47:52 +0100

    frontend szukseg proba endpoint

--- src/main/java/com/auo/backend/controllers/AuthenticationController.java
+++ src/main/java/com/auo/backend/controllers/AuthenticationController.java
@@ -2,10 +2,11 @@ package com.auo.backend.controllers;
 
 import com.auo.backend.dto.UserLoginDto;
 import com.auo.backend.dto.UserRegisterDto;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.auo.backend.models.User;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping("api/v1/auth")
@@ -17,5 +18,10 @@ public class AuthenticationController {
     @PostMapping("/register")
     public String Register(@RequestBody UserRegisterDto userRegisterDto) {
         return "jwttokentobeimplemented"+userRegisterDto.getUsername()+userRegisterDto.getPassword()+userRegisterDto.getEmail()+userRegisterDto.getNickname()+userRegisterDto.getDate_of_birth();
+    }
+
+    @PostMapping("/profile")
+    public User Profile(@RequestBody String token) {
+        return new User("username","pw nem kell","nick",2,token,true,null,null, LocalDate.parse("2001-09-11"));
     }
 }
--- src/main/java/com/auo/backend/models/User.java
+++ src/main/java/com/auo/backend/models/User.java
@@ -6,13 +6,17 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "app_user")
-public class User {
+public class User implements UserDetails {
 
     @Getter
     @Id
@@ -107,4 +111,28 @@ public class User {
         this.date_of_birth = date_of_birth;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
