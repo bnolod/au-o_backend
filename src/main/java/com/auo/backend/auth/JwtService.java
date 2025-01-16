5a76b7d6ef4adb94706d6d9dd268c5a913e2f144
bnolod <sadfoolc@gmail.com>
2025-01-16 15:27:31 +0100

    gradle jo + jwt update 1:02

--- src/main/java/com/auo/backend/auth/JwtService.java
+++ src/main/java/com/auo/backend/auth/JwtService.java
@@ -1,10 +1,32 @@
 package com.auo.backend.auth;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
+
+    private static final String SECRET_KEY = "B939C36C679A134ACAAEE2F82DBBA";
+
     public String extractUsername(String token) {
         return null;
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
