b5e99717aef64e719d5da4f43f1cbd60b6ac85b8
bnolod <sadfoolc@gmail.com>
2025-01-17 13:04:16 +0100

    validation regexek

--- src/main/java/com/auo/backend/validationServices/UserValidationService.java
+++ src/main/java/com/auo/backend/validationServices/UserValidationService.java
@@ -3,27 +3,39 @@ package com.auo.backend.validationServices;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.regex.Pattern;
 
 @Service
 public class UserValidationService {
 
+    public static boolean patternMatches(String input, String regexPattern) {
+        return Pattern.compile(regexPattern)
+                .matcher(input)
+                .matches();
+    }
+
     public boolean IsEmailValid(String email) {
-        return true;
+        String regexPattern = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
+        return patternMatches(email, regexPattern);
     }
     public boolean IsUsernameValid(String username) {
-        return true;
+        String regexPattern = "^(?=.*[a-zA-Z])[a-zA-Z0-9]{3,20}$";
+        return patternMatches(username,regexPattern);
     }
     public boolean IsPasswordValid(String password) {
-        return true;
-    }
+        String regexPattern ="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[A-Za-z\\d]{8,64}$";
+        return patternMatches(password,regexPattern)
+;    }
     public boolean IsDateOfBirthValid(LocalDate dateofbirth) {
-        return true;
+        return dateofbirth.isBefore(LocalDate.now().minusYears(16));
     }
     public boolean IsNicknameValid(String nickname) {
-        return true;
+        String regexPattern = "^(?=.*[a-zA-Z])[a-zA-Z0-9]{3,20}$";
+        return patternMatches(nickname,regexPattern);
     }
-    public boolean IsBioValid() {
-        return true;
+    public boolean IsBioValid(String bio) {
+        String regexPattern = "^.{0,100}$";
+        return patternMatches(bio, regexPattern);
     }
 
 }
